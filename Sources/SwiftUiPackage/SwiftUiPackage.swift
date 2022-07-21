a6203a0f30448fb4599b97ee0bca010369865a0e
hohieuu <hohieu1099@gmail.com>
2022-07-21 20:43:00 +0700

    v3

--- Sources/SwiftUiPackage/SwiftUiPackage.swift
+++ Sources/SwiftUiPackage/SwiftUiPackage.swift
@@ -1,6 +1,18 @@
+import SwiftUI
+import Foundation
+
 public struct SwiftUiPackage {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+   
+    struct HieuUIView: View {
+        @available(macOS 10.15, *)
+        @available(iOS 13, *)
+        var body: some View {
+            Text("Hello, World!")
+        }
     }
 }
