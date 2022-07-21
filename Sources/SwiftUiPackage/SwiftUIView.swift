b3362047127900c5161afc7c402ff77ef6df04a1
hohieuu <hohieu1099@gmail.com>
2022-07-21 21:05:50 +0700

    v10

--- Sources/SwiftUiPackage/SwiftUIView.swift
+++ Sources/SwiftUiPackage/SwiftUIView.swift
@@ -2,8 +2,8 @@ import SwiftUI
 import Foundation
 
 @available(macOS 10.15, *)
-public struct HieuUIView: View {
-    public var body: some View { 
+struct HieuUIView: View {
+    var body: some View { 
         Text("Hello, World!")
     }
 }
