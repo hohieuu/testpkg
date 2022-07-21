99f0a17c6031f7d3b610d7413799b957f991e035
hohieuu <hohieu1099@gmail.com>
2022-07-21 21:13:03 +0700

    1

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+public struct MyLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
+
+public struct SwiftUIView: View {
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
