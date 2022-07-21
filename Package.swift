1e5ce6250f8b5f0342c49a44058a4d68fd20ea30
hohieuu <hohieu1099@gmail.com>
2022-07-21 21:05:06 +0700

    v9

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftUiPackage",
-            targets: ["SwiftUiPackage"]),
+            targets: ["SwiftUiPackage"],
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftUiPackage",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "SwiftUiPackageTests",
             dependencies: ["SwiftUiPackage"]),
